388223a71254229a9b1c69518cad48c6bfdd3366
Keely Sherman <keelysherman@Keelys-MacBook-Pro.local>
2023-04-19 15:59:35 -0400

    i just realized i havent pushed once

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,18 +1,91 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (condition) {
+    
+  } else {
+    
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (condition) {
+    
+  } else {
+    
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (condition) {
+    
+  } else {
+    
+  }
+}
 
 // TODO: Create a function to generate markdown for README
+//if this doesnt work change data parameters to (description,instalation,usage,etc)
 function generateMarkdown(data) {
   return `# ${data.title}
+
+  ## Description
+  
+  ${data.description}
+  
+  ## Table of Contents 
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+  - [Questions](#questions)
+  
+  ## Installation
+ 
+  ${data.installation}
+  
+  ## Usage
+  
+  ${data.usage}
+  
+  
+  ## Credits
+  
+  ${data.credits}
+  
+  ## License
+  
+  The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+  
+  ## Badges
+  
+  ![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
+  
+  Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+  
+  ## Features
+  
+  ${data.features}
+  
+  ## How to Contribute
+  
+  If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+
+  ## Testing
+
+  ${data.test}
+
+  ##Questions
+
+  If you have any Questions and would like to reach me, my information is below!
+  
+  ${data.userName}
+  ${data.email}
 
 `;
 }
--- index.js
+++ index.js
@@ -1,13 +1,158 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+{//10
+    type:'input',
+    message: 'What is your gitHub username?',
+    name: 'userName',
+    validate: userNameInput =>{
+        if (userNameInput) {
+            return true;
+        } else {
+            console.log('Please input your GitHub Username!');
+            return false;
+        }
+    }
+},
+{//11
+    type:'input',
+    message: 'What is your email?',
+    name: 'email',
+    validate: emailInput =>{
+        if (emailInput) {
+            return true;
+        } else {
+            console.log('Please input your email!');
+            return false;
+        }
+    }
+},
+{//1
+    type:'input',
+    message: 'What is your project name?',
+    name: 'title',
+    validate: titleInput =>{
+        if (titleInput) {
+            return true;
+        } else {
+            console.log('Please input your project name!');
+            return false;
+        }
+    }
+},
+{//2
+    type:'input',
+    message: 'What is your motivation for creating this project/Why did you create this project? What does it do/solve? What did you learn?',
+    name: 'description',
+    validate: descriptionInput =>{
+        if (descriptionInput) {
+            return true;
+        } else {
+            console.log('Please describe why you created this project!');
+            return false;
+        }
+    }
+},
+{//4
+    type:'input',
+    message: 'How does someone go about using your project?',
+    name: 'usage',
+    validate: usageInput =>{
+        if (usageInput) {
+            return true;
+        } else {
+            console.log('Please provide steps to using your project!');
+            return false;
+        }
+    }
+},
+{//5
+    type:'input',
+    message: 'Who worked on this project with you? Format your answers as Name[githubusername](link to their github).',
+    name: 'credit',
+    validate: creditInput =>{
+        if (creditInput) {
+            return true;
+        } else {
+            console.log('Please give credit to all contributors!');
+            return false;
+        }
+    }
+},
+{//8
+    type:'input',
+    message: 'What languages/features did you use on this project?',
+    name: 'features',
+    validate: featuresInput =>{
+        if (featuresInput) {
+            return true;
+        } else {
+            console.log('Please provide all languages and/or features used in this project!');
+            return false;
+        }
+    }
+},
+{//3
+    type:'input',
+    message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.',
+    name: 'installation',
+    validate: installationInput =>{
+        if (installationInput) {
+            return true;
+        } else {
+            console.log('Please choose a license!');
+            return false;
+        }
+    }
+},
+{//6
+    type:'checkbox',
+    message: 'Choose a license for your project (required).',
+    name: 'license',
+    choices: ['Apache', 'MIT', 'Mozilla-Public', 'GNU-General-Public', 'Common-Development-and Distribution', 'None'],
+    validate: licenseInput =>{
+        if (licenseInput) {
+            return true;
+        } else {
+            console.log('Please choose a license!');
+            return false;
+        }
+    }
+},
+{//9
+    type:'input',
+    message: 'How do you test your project?',
+    name: 'test',
+    validate: testInput =>{
+        if (testInput) {
+            return true;
+        } else {
+            console.log('Please provide directions to test your project');
+            return false;
+        }
+    }
+}
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err)=>{
+        if (err) 
+            return console.log('Sorry, there was an error: ' + err);
+    console.log('Great Job! Your information has been transferred to the README')
+    })
+}
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+   inquirer.prompt(questions)
+   .then(function(userInput){
+        console.log(userInput)
+        writeToFile('README.md', generateMarkdown(userInput))
+   })
+}
 
 // Function call to initialize app
 init();
